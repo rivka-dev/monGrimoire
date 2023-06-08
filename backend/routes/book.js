3d4e1860a591d309608ac26d6d6316bf6ecc4c78
Rivka Kauffmann <rivkauffmann@gmail.com>
2023-06-08 11:34:35 +0200

    suppression de multer dans routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
 const sharpConfig=require('../middleware/sharp-config')
 
 const bookCtrl = require('../controllers/book');
