b8f796f9ef1c8b99bd64942daefdc53afeafb342
Rivka Kauffmann <rivkauffmann@gmail.com>
2023-06-08 22:30:27 +0200

    ajout de .env

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const {cors}=require('./middleware/cors')
 const app = express();
 
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
+require('dotenv').config()
 
-mongoose.connect('mongodb+srv://rivka:Rivka.97@bookstore.eus7dy8.mongodb.net?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -16,12 +18,7 @@ mongoose.connect('mongodb+srv://rivka:Rivka.97@bookstore.eus7dy8.mongodb.net?ret
 
 app.use(express.json());//pour avoir acces au corps de la requete json
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors)
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
