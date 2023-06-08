c79add4436e98c09fe37fe7f841e242eec8007bc
Rivka Kauffmann <rivkauffmann@gmail.com>
2023-06-08 09:55:34 +0200

    ajout des dossiers

---
+++ backend/middleware/sharp-config.js
@@ -0,0 +1,25 @@
+const sharpConfig = require ('sharp')
+const SharpMulter=require('sharp-multer')
+const multer=require ('multer')
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/webp':'webp'
+};
+const storage =  
+ SharpMulter ({
+              destination:(req, file, callback) =>callback(null, "images"),
+              filename: (req, file, callback) => {
+                const name = file.originalname.split(' ').join('_');
+                const extension = MIME_TYPES[file.mimetype];
+                callback(null, name + Date.now() + '.' + extension);
+              },
+              imageOptions:{
+               fileFormat: "webp",
+               quality: 20,
+               
+                 }
+           });
+           module.exports = multer({storage: storage}).single('image');
---
+++ backend/routes/book.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const sharpConfig=require('../middleware/sharp-config')
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBooks);
+router.post('/', auth,  sharpConfig,bookCtrl.createBook);
+router.get('/bestrating',  bookCtrl.getBestRating);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', auth, sharpConfig, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating',auth,bookCtrl.createRating)
+
+module.exports = router;
