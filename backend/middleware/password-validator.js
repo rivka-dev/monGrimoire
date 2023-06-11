dadfac20e356dc8df726c97d271650f87d80bd6e
Rivka Kauffmann <rivkauffmann@gmail.com>
2023-06-11 09:51:08 +0200

    password validator

---
+++ backend/middleware/password-validator.js
@@ -0,0 +1,22 @@
+const passwordValidator = require('password-validator');
+
+
+const schema = new passwordValidator();
+
+
+schema
+.is().min(8)                                    
+.is().max(100)                                  
+.has().uppercase()                              
+.has().lowercase()  
+.has().digits()                                                           
+.has().not().spaces()                           
+
+module.exports= ((req, res, next) => {
+    const password = req.body.password;
+    if (!schema.validate(password)) {
+        return res.status(400).json({ error: `Attention, mot de passe trop faible `+ schema.validate(password, {list: true }) })
+    } else {
+        next()
+    };
+  });
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const passwordValidator=require('../middleware/password-validator')
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
